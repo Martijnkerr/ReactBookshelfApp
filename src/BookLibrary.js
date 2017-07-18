d34592aebde9cc7e8f3289e18381f44f37befe4b
Martijn Kerckhaert <martijn_kerckhaert@hotmail.com>
2017-07-18 20:24:35 +0200

    refactor filtering books

--- src/BookLibrary.js
+++ src/BookLibrary.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Header from './Header'
 import Bookshelf from './Bookshelf'
+import * as BooksAPI from '../src/BooksAPI'
+
 
 class BookLibrary extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
   render() {
     return (
       <div>
         <div className="list-books">
           <Header />
           <Bookshelf
-            shelfName="Currently reading"
+            shelfName="Want to read"
+            books={this.state.books.filter((b) => b.shelf === "wantToRead")}
           />
           <Bookshelf
-            shelfName="Want to read"
+            shelfName="Currently reading"
+            books={this.state.books.filter((b) => b.shelf === "currentlyReading")}
            />
           <Bookshelf
             shelfName="Read"
+            books={this.state.books.filter((b) => b.shelf === "read")}
            />
           <div className="open-search">
             <Link to="/search" />
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,46 +1,21 @@
 import React, { Component } from 'react'
 import Book from './Book'
-import * as BooksAPI from '../src/BooksAPI'
-
-
 
 class Bookshelf extends Component {
-
-  state = {
-    books: []
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-  }
-
-
-
   render() {
-    let booksForShelf
-    if (this.props.shelfName === "Want to read") {
-      booksForShelf = this.state.books.filter((book) => book.shelf === "wantToRead")
-    } else if (this.props.shelfName === "Currently Reading") {
-      booksForShelf = this.state.books.filter((book) => book.shelf === "currentlyReading")
-    } else {
-      booksForShelf = this.state.books.filter((book) => book.shelf === "read")
-    }
+    const { books } = this.props
     return (
       <div>
         <div className="bookshelf">
           <h2 className="bookshelf-title">{this.props.shelfName}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              {booksForShelf.map(function(book, index){
-                return (
-                  <Book
-                    key={book.id}
-                    book={book}
-                  />
-                )
-              })}
+            {books.map((book) =>
+              <Book
+              key={book.id}
+              book={book}
+              />
+            )}
             </ol>
           </div>
         </div>
